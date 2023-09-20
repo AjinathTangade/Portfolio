32150f4ed7d6041cf8bf9e5af4dd5b1661758ee9
@Ajinath Tangade <93082482+AjinathTangade@users.noreply.github.com>
2023-09-20 12:38:07 +0000

    CTA done

--- portfolio/src/component/CtaSection.jsx
+++ portfolio/src/component/CtaSection.jsx
@@ -4,14 +4,14 @@ export function CtaSection() {
     return (
         <section>
 
-            <div className="lg:flex mx-auto max-w-7xl flex items-center justify-between py-5 sm:px-6 sm:block ">
-                <div className="w-full lg:w-w-3/5 lg:h-3/4">
+            <div className="lg:flex mx-auto max-w-7xl h-[600px] flex items-center justify-between py-5 sm:px-6 sm:block ">
+                <div className="w-full lg:w-4/6">
                     <div className="my-10 lg:my-0">
-                        <p className='text-emerald-300'>Hi, my name is</p>
-                        <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
-                            Hello, I’m Ajinath Tangade, <br/> Front-end Developer and Web Devloper.
+                        <p className='text-emerald-300 text-lg font-semibold pb-4'>Hi, my name is</p>
+                        <h2 className="lg:text-6xl font-bold leading-8 text-indigo-100	sm:text-4xl">
+                            Hello, I’m <span className='text-emerald-300 font-semibold'>Ajinath Tangade, </span> <br /> Front-end Developer <br /> and Web Developer.
                         </h2>
-                        <p className="mt-4 max-w-xl text-base leading-relaxed text-gray-600">
+                        <p className="mt-4 max-w-xl text-base leading-relaxed text-indigo-50 font-semibold">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nesciunt eos facilis
                             debitis voluptas iure consectetur odit fugit voluptate recusandae?
                         </p>
@@ -19,23 +19,19 @@ export function CtaSection() {
                         <form action="#" method="POST" className="mt-8 max-w-xl">
                             <div className="flex flex-col sm:flex-row sm:items-center">
                                 <div className="flex w-full max-w-sm items-center space-x-2">
-                                    <input
-                                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 "
-                                        type="email"
-                                        placeholder="Email"
-                                    />
+
                                     <button
                                         type="button"
-                                        className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                                        className="rounded-md border border-emerald-300 px-8 py-3 text-base font-semibold text-emerald-300 hover:shadow-lg hover:shadow-cyan-500/50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
                                     >
-                                        Subscribe
+                                        Resume
                                     </button>
                                 </div>
                             </div>
                         </form>
                     </div>
                 </div>
-                <div className="w-full lg:w-2/5"></div>
+                <div className="w-full lg:w-1/3"></div>
             </div>
         </section>
     )
